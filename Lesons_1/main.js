1e749d94482dd9ab2ddbf320f76f89359222d06b
Tanya <tchirska@gmail.com>
2023-08-16 16:59:49 +0300

    add first HomeWork

---
+++ Lesons_1/main.js
@@ -0,0 +1,35 @@
+// 1
+let a='hello'
+let b='owu'
+let c='com'
+let d='ua'
+let e=1
+let f=10
+let g=-999
+let h = 123
+let j=3.14
+let i =2.7
+let k= 16
+let l=true
+let m=false
+console.log(a,b,c,d,e,f,g,h,j,i,k,l,m)
+
+// 2
+let firstName="Тетяна"
+let middleName='Володимирівна'
+let lastName='Чирська'
+console.log(lastName+' '+firstName+' '+middleName)
+//3
+let  a1=100
+let b1='100'
+let c1=true
+console.log(typeof(a1),typeof(b1),typeof(c1))
+
+
+// додаткове
+
+let firstname=prompt('enter your name')
+let middlename=prompt('enter middleName')
+let lastname=prompt('enter lastName')
+console.log('Hi'+' '+firstname+' '+middlename+' '+lastname)
+
